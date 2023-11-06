d8c5e04414185dc71dd5cc4c34aa21564b1ea89a
Xepobopa <ismartsdn4477@gmail.com>
2023-11-06 22:17:34 +0200

    init

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-const socket = io("https://videcall-server.vercel.app/");
+const socket = io("https://videcall-server-9tfx.vercel.app/");
 
 export default socket
